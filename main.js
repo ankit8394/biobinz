60444b2c82300276dd1f81cae6b530d575ce622c
Ankit <ankit0839483@gmail.com>
2024-12-17 22:56:32 +0530

    final

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const navLinks = document.querySelectorAll("nav ul li a");
 
-    navLinks.forEach(link => {
-        link.addEventListener("click", (e) => {
-            navLinks.forEach(item => item.classList.remove("active"));
-            e.target.classList.add("active");
-        });
-    });
-});
 $(document).ready(function(){
     $(".menu-btn").click(function(){
       $("#list").slideToggle("slow");
